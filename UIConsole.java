34e0db3dbc877065bb983ba497004d0ab76f6c47
Iryna Karseka <Karsira88@mail.ru>
2023-10-23 13:29:09 +0300

    New file

---
+++ UIConsole.java
@@ -0,0 +1,25 @@
+
+
+public class UIConsole {
+    public static void showGreet() {
+        System.out.println("\n============================================================");
+        System.out.println("\tРозыгрыш игрушек в магазине детских таваров! ");
+        System.out.println("\t   Получите шанс выиграть игрушку!!!");
+        System.out.println("============================================================");
+    }
+
+    public static void showControlLevers() {
+        System.out.println("\n\t\t1 - Участвовать в розыгрыше");
+        System.out.println("\t\t2 - Просмотреть призы");
+        System.out.println("\n============================================================");
+        System.out.print("Введите цифру для выбора сюда ==> ");
+    }
+
+    public static void showErrorEnteringIncorrectly() {
+        System.out.println("\n\n\n\t\t   Неправильный ввод!\n");
+    }
+        
+    public static void showAuthor() {
+        System.out.println("\n\n\n\n\n\t\t(c)2023 Ирина Карсеко для GB\n\n\n");
+    }
+}
